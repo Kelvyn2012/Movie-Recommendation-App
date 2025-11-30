b95a2a12eda3c270e8c136d624ad4706d784676a
kelvyn2012 <kudufo@gmail.com>
2025-11-30 18:02:05 +0100

    updated my CORS

--- movie_recomendation/settings.py
+++ movie_recomendation/settings.py
@@ -16,7 +16,12 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-g&&&lls*dnv_uz57)!(&^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=Csv())
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    '.onrender.com',
+    'movie-recommendation-app-uskd.onrender.com',
+]
 
 
 # Application definition
